c36737e1bc2bea952f487a856330d19bb6974dd8
Labriever <liveinmars2018@gmail.com>
2024-07-15 12:52:57 +0900

    feat: lotto model, util, view에 클래스 생성. util에 Argument 생성 후 예외처리 관리할 예정

---
+++ src/main/java/lotto/util/LottoArgument.java
@@ -0,0 +1,29 @@
+package lotto.util;
+
+import java.util.List;
+
+public class LottoArgument {
+	
+	// 로또 번호는 1~45 사이
+	public static void lottoRange(List<Integer> number) {
+		for(int lotto : number) {
+			if(lotto < 1 || lotto > 45) {
+				throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+			}
+		}
+	}
+	
+	// 로또 구매는 1000원 단위로만 구매할 수 있으며, 100원 단위가 나오면 예외처리/
+	public static void buyLotto(int pay) {
+		if(pay % 1000 != 0) {
+			throw new IllegalArgumentException("1000원 단위로만 구매할 수 있습니다.");
+		}
+	}
+	
+	// 로또 번호의 갯수는 총 6개
+	public static void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
